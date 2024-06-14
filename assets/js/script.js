76e9dc5cf7fd1878b6d9e3d704af53a2b71ce736
randi089 <randifebriadi@gmail.com>
2024-06-14 17:11:03 +0700

    menambahkan alert hapus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
+// flash data
 const flashdata = $('.flash-data').data('flashdata');
 if (flashdata) {
     new Swal({
         title: 'Data Mahasiswa',
         text: 'Berhasil ' + flashdata,
-        icon: 'success'
+        icon: 'success',
+        confirmButtonColor: "#007BFF"
     });
 }
+
+// tombol hapus
+$('.hapus').on('click', function (e) {
+
+    e.preventDefault();
+    const href = $(this).attr('href');
+
+   new Swal({
+    title: "Apakah anda yakin",
+    text: "data mahasiswa akan dihapus",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#007BFF",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Hapus Data!"
+   }).then((result) => {
+    if (result.isConfirmed) {
+      document.location.href = href;
+    }
+  });; 
+});
